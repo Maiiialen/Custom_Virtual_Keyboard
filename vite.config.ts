62a42caa9e928274289b07e39e451b3f0c2c86a2
Maiiialen <magdalena.salega@gmail.com>
2023-09-26 16:05:48 +0200

    fix: prepare project to export to npm library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), dts({ insertTypesEntry: true }), libInjectCss()],
+  build: {
+    lib: {
+      name: "custom-virtual-keyboard",
+      fileName: "custom-virtual-keyboard",
+      entry: resolve("src/App.tsx")
+    }
+  }
+});
