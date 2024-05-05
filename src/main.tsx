cde40fa52bdb674e51db8f8f1f30b61a9ae53915
Maiiialen <magdalena.salega@gmail.com>
2024-05-05 20:16:24 +0200

    ref: to enable generating github page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
 import Example from "./pages/Example";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
